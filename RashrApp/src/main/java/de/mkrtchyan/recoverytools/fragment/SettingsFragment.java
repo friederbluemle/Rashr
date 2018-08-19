a80df9a8d558a32c2ef10a1cfd16c84368a6509e
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-08-19 16:55:54 +0800

    Fix misplaced Javadoc/file headers

--- RashrApp/src/main/java/de/mkrtchyan/recoverytools/fragment/SettingsFragment.java
+++ RashrApp/src/main/java/de/mkrtchyan/recoverytools/fragment/SettingsFragment.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017 Aschot Mkrtchyan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package de.mkrtchyan.recoverytools.fragment;
 
 import android.content.Context;
@@ -23,26 +45,6 @@ import de.mkrtchyan.recoverytools.ReportDialog;
 import de.mkrtchyan.utils.Common;
 import de.psdev.licensesdialog.LicensesDialog;
 
-/**
- * Copyright (c) 2017 Aschot Mkrtchyan
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * <p/>
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * <p/>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 public class SettingsFragment extends PreferenceFragmentCompat {
 
     public static void showChangelog(Context AppContext) {
--- Utils-Library/src/main/java/de/mkrtchyan/utils/Unzipper.java
+++ Utils-Library/src/main/java/de/mkrtchyan/utils/Unzipper.java
@@ -1,7 +1,6 @@
-package de.mkrtchyan.utils;
-
 /*
- * Copyright (c) 2013 Ashot Mkrtchyan
+ * Copyright (c) 2013 Aschot Mkrtchyan
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -9,8 +8,8 @@ package de.mkrtchyan.utils;
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,6 +19,8 @@ package de.mkrtchyan.utils;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+package de.mkrtchyan.utils;
 
 import android.os.AsyncTask;
 import android.util.Log;
